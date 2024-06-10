7c53e4fa15d8ee4e3737864fa5efa8fdb45b0b61
rasheeda <raskieda@gmail.com>
2024-06-10 16:46:06 +0200

    added setError and applied styling

--- my-app/src/Fetch.jsx
+++ my-app/src/Fetch.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function Fetch() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -15,23 +16,41 @@ export default function Fetch() {
         console.log(data);
         setPosts(data);
       })
-      .catch((error) => console.error(error.message)); // Log the error to the console
+      .catch((error) => {
+        console.error(error.message);
+        setError(error.message);
+      });
   }, []);
 
   return (
     <div>
-      <h2>Posts</h2>
-      <ul style={{ listStyleType: "none" }}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <span>{post.id}. </span>
-            <strong>{post.title}</strong>
-            <small>
-              <p>{post.body}</p>
-            </small>
-          </li>
-        ))}
-      </ul>
+      {error ? (
+        <strong>
+          <div
+            style={{
+              fontSize: "larger",
+              marginTop: "10em",
+            }}
+          >
+            Data fetching failed
+          </div>
+        </strong>
+      ) : (
+        <div>
+          <h2>Posts</h2>
+          <ul style={{ listStyleType: "none" }}>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <span>{post.id}. </span>
+                <strong>{post.title}</strong>
+                <small>
+                  <p>{post.body}</p>
+                </small>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
